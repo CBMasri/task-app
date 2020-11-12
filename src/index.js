a8e30fe78268f087ca89a6f0c9d2e4173bed41ef
Carl Masri <carl.masri@gmail.com>
2020-11-11 18:26:20 -0800

    Update project directory structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './assets/styles/index.css';
 import App from './App';
 
+// Mount the app in strict mode, which gives us some
+// additional checks/warnings while running in dev
+// https://reactjs.org/docs/strict-mode.html
 ReactDOM.render(
   <React.StrictMode>
     <App />
