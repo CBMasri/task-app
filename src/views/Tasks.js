2fe044543b2577f24335b1040a86996a8313dc76
Carl Masri <carl.masri@gmail.com>
2020-11-14 19:45:05 -0800

    Add a button to clear all tasks

--- src/views/Tasks.js
+++ src/views/Tasks.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import { DragDropContext } from 'react-beautiful-dnd'
 
 import Lane from 'components/Lane.js'
@@ -12,15 +12,32 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
 `
+const Controls = styled.div`
+  display: flex;
+  width: 782px;
+  justify-content: space-between;
+  font-size: 16px;
+  border-radius: 5px;
+  margin-bottom: 15px;
+`
 const AddTask = styled.input`
+  color: #e9e9e9;
   width: 300px;
   padding: 8px;
-  margin-bottom: 20px;
-  border-radius: 5px;
   border: 2px solid #aaaaaa;
-  background-color: #282c34;
-  color: #e9e9e9;
-  font-size: 16px;
+  background-color: inherit;
+`
+  const ClearTasks = styled.button`
+  padding: 8px;
+  border: 2px dashed #757575;
+  background-color: inherit;
+  color: #757575;
+  margin-left: 20px;
+  &:hover {
+    background-color: #b92424;
+    cursor: pointer;
+    color: #e9e9e9;
+  }
 `
 const Lanes = styled.div`
   display: flex;
@@ -35,22 +52,28 @@ const Lanes = styled.div`
  * into which tasks can be organized.
  */
 function Tasks() {
-  const cachedData = localStorage.getItem('data')
-  const data = cachedData ? JSON.parse(cachedData) : initialData
+  const cache = localStorage.getItem('data')
+  const data = cache ? JSON.parse(cache) : initialData
 
   const [ tasks, setTasks ] = useState(data.tasks)
   const [ lanes, setLanes ] = useState(data.lanes)
   const [ inputValue, setInputValue ] = useState('')
 
+  const addTaskRef = useRef(null)
   const laneOrder = Object.keys(lanes)
 
-  // Cache state in localStorage
+  // Persist state in localStorage
   useEffect(() => {
     localStorage.setItem('data', JSON.stringify({
       'tasks': tasks,
       'lanes': lanes
     }))
   })
+
+  // Auto-focus add task input on inital mount
+  useEffect(() => {
+    addTaskRef.current.focus()
+  }, [])
 
   /**
    * Handle the 'Enter' keypress event
@@ -63,6 +86,19 @@ function Tasks() {
       addTask(inputValue)
       setInputValue('')
     }
+  }
+
+  /**
+   * Remove all the tasks from the
+   * lanes and delete them.
+   */
+  function handleClearTasks() {
+    const updatedLanes = { ...lanes }
+    for (const laneId in updatedLanes) {
+      updatedLanes[laneId].taskIds = []
+    }
+    setLanes(updatedLanes)
+    setTasks({})
   }
 
   /**
@@ -120,6 +156,14 @@ function Tasks() {
     setTasks(updatedTasks)
   }
 
+  /**
+   * onDragEnd handler for react-beautiful-dnd.
+   *
+   * Required by DragDropContext, this function
+   * controls the state of the drag-n-drop items.
+   *
+   * @param {Object} result
+   */
   function onDragEnd(result) {
     const { destination, source, draggableId } = result
 
@@ -147,12 +191,18 @@ function Tasks() {
 
   return (
     <Container>
-      <AddTask
-        placeholder="What needs to be done?"
-        value={inputValue}
-        onKeyPress={handleKeyPress}
-        onChange={event => setInputValue(event.target.value)}
-      />
+      <Controls>
+        <AddTask
+          ref={addTaskRef}
+          placeholder="What needs to be done?"
+          value={inputValue}
+          onKeyPress={handleKeyPress}
+          onChange={event => setInputValue(event.target.value)}
+        />
+        <ClearTasks onClick={() => handleClearTasks()}>
+          Clear Tasks
+        </ClearTasks>
+      </Controls>
       <DragDropContext onDragEnd={onDragEnd}>
         <Lanes>
           {laneOrder.map(laneId => {
