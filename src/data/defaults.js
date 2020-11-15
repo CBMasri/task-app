db7673149687530314eccd947c6ddc2513e524e6
Carl Masri <carl.masri@gmail.com>
2020-11-14 16:18:49 -0800

    Clean up initialData structure

--- src/data/defaults.js
+++ src/data/defaults.js
@@ -1,25 +1,27 @@
-const tasks = {
-  'task-1': { id: 'task-1', content: 'Drink coffee' },
-  'task-2': { id: 'task-2', content: 'Get a new job' },
-  'task-3': { id: 'task-3', content: 'Buy a house' },
+// Task lanes will be pre-populated with this data
+const initialData = {
+  tasks: {
+    'task-1': { id: 'task-1', content: 'Drink coffee' },
+    'task-2': { id: 'task-2', content: 'Get a new job' },
+    'task-3': { id: 'task-3', content: 'Buy a house' }
+  },
+  lanes: {
+    'lane-1': {
+      id: 'lane-1',
+      title: 'To Do',
+      taskIds: [ 'task-3' ]
+    },
+    'lane-2': {
+      id: 'lane-2',
+      title: 'In Progress',
+      taskIds: [ 'task-2' ]
+    },
+    'lane-3': {
+      id: 'lane-3',
+      title: 'Done',
+      taskIds: [ 'task-1' ]
+    }
+  }
 }
-const lanes = {
-  'lane-1': {
-    id: 'lane-1',
-    title: 'To Do',
-    taskIds: [ 'task-3' ]
-  },
-  'lane-2': {
-    id: 'lane-2',
-    title: 'In Progress',
-    taskIds: [ 'task-2' ]
-  },
-  'lane-3': {
-    id: 'lane-3',
-    title: 'Done',
-    taskIds: [ 'task-1' ]
-  },
-}
-const initialData = { tasks, lanes }
 
 export default initialData
